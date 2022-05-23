ff83457b3377f86964c14bf15e23050aa315be49
sunzengguang <424160958@qq.com>
2022-05-23 16:18:32 +0800

    ...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import App from "./views/App";
+import App from "./views/App.vue";
 new Vue({
     render: h => h(App)
 }).$mount('#app')
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,38 @@ const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
 const html = require('html-webpack-plugin')
 module.exports = {
+    mode: 'development',
     entry:'./src/index.js',
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'my-webpack-bundle'
     },
-    modules: {
+    module: {
         rules: [
             {
                 test: /^.*\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /^.*\.vue$/,
+                use: ['vue-loader']
             }
         ]
     },
     plugins:[
         new VueLoaderPlugin(),
         new html({
-            template: ``
+            template: "./public/index.html"
         })
     ],
     devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
         port: 9999
-    }
+    },
+    // resolve: {
+    //     extensions: ['.js','.vue']
+    // }
 }
